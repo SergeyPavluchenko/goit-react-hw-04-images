0fee4c8a485e53552c2b4e9205614f00ce78ca2b
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2023-01-27 23:55:39 +0200

    modal

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -4,44 +4,35 @@ import {
   ImageGalleryItemImg,
 } from 'components/ImageGalleryItem/ImageGalleryItem.styled';
 import { Modal } from 'components/Modal/Modal';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+export const ImageGalleryItem = ({ id, webformatURL, largeImageURL, tags }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
   };
 
-  static propTypes = {
-    id: PropTypes.number,
-    webformatURL: PropTypes.string.isRequired,
-    largeImageURL: PropTypes.string.isRequired,
-    tags: PropTypes.string.isRequired,
-  };
+  return (
+    <>
+      <ImageGalleryItemLi key={id} onClick={toggleModal}>
+        <ImageGalleryItemImg src={webformatURL} width="400" alt={tags} />
+      </ImageGalleryItemLi>
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
+      {showModal && (
+        <Modal
+          largeImageURL={largeImageURL}
+          tags={tags}
+          onClose={toggleModal}
+        />
+      )}
+    </>
+  );
+};
 
-  render() {
-    const { id, webformatURL, largeImageURL, tags } = this.props;
-    const { showModal } = this.state;
-
-    return (
-      <>
-        <ImageGalleryItemLi key={id} onClick={this.toggleModal}>
-          <ImageGalleryItemImg src={webformatURL} width="400" alt={tags} />
-        </ImageGalleryItemLi>
-
-        {showModal && (
-          <Modal
-            largeImageURL={largeImageURL}
-            tags={tags}
-            onClose={this.toggleModal}
-          />
-        )}
-      </>
-    );
-  }
-}
+ImageGalleryItem.propTypes = {
+  id: PropTypes.number,
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+};
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,39 +1,37 @@
 import { WindowModal, Overlay } from 'components/Modal/Modal.styled';
 
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-export class Modal extends Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeModalByEsc);
-  }
+export const Modal = ({ largeImageURL, tags }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', closeModalByEsc);
+    return () => {
+      window.removeEventListener('keydown', closeModalByEsc);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModalByEsc);
-  }
-
-  closeModalByEsc = event => {
+  const closeModalByEsc = event => {
     if (event.code === 'Escape') {
       this.props.onClose();
     }
   };
-  closeModalOverlay = event => {
+  const closeModalOverlay = event => {
     if (event.currentTarget === event.target) {
       this.props.onClose();
     }
   };
 
-  render() {
-    const { largeImageURL, tags } = this.props;
-    return (
-      <Overlay onClick={this.closeModalOverlay}>
-        <WindowModal>
-          <img src={largeImageURL} alt={tags} width="850" />
-        </WindowModal>
-      </Overlay>
-    );
-  }
-}
+  // const { largeImageURL, tags } = this.props;
+  return (
+    <Overlay onClick={closeModalOverlay}>
+      <WindowModal>
+        <img src={largeImageURL} alt={tags} width="850" />
+      </WindowModal>
+    </Overlay>
+  );
+};
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
